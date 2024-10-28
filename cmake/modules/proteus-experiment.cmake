4646b8f22aded428599b737d1e9abb2f28cca1d6
Hamish Nicholson <hmnicholson12@gmail.com>
2024-10-28 10:16:57 +0100

    [build] make proteus-experiment a CMake function
    A step torwards function-based cmake. This will make it much easier
    to define multiple executables in the same folder.

--- cmake/modules/proteus-experiment.cmake
+++ cmake/modules/proteus-experiment.cmake
@@ -1,12 +1,30 @@
-set(_proteus_install_target ${PROJECT_NAME})
+# Function to configure a Proteus experiment target.
+#
+# This function sets up the necessary configurations for a given target
+# to be used as a Proteus experiment. It enables default warnings for the
+# target and includes the _proteus-install module to handle installation
+# specifics.
+#
+# Arguments:
+#   target_name - The name of the target to configure.
+#
+# Example usage:
+#   configure_proteus_experiment(my_target)
+#
+# Note:
+#   This function should be called after the target has been defined.
+#   _proteus-install should also be converted to a function
+function(configure_proteus_experiment target_name)
+  set(_proteus_install_target ${target_name})
 
-#set(_proteus_install_dev ${PROJECT_NAME}_Development)
-#set(_proteus_install_bin ${PROJECT_NAME}_Experiments)
-#set(_proteus_install_dev ${PROJECT_NAME}_${PROTEUS_CPACK_COMP_SUFFIX_DEV})
-#set(_proteus_install_bin ${PROJECT_NAME}_${PROTEUS_CPACK_COMP_SUFFIX_EXPERIMENTS})
+  # set(_proteus_install_dev ${target_name}_Development)
+  # set(_proteus_install_bin ${target_name}_Experiments)
+  # set(_proteus_install_dev ${target_name}_${PROTEUS_CPACK_COMP_SUFFIX_DEV})
+  # set(_proteus_install_bin ${target_name}_${PROTEUS_CPACK_COMP_SUFFIX_EXPERIMENTS})
 
-target_enable_default_warnings(${PROJECT_NAME})
+  target_enable_default_warnings(${target_name})
 
-set(_proteus_install_dev ${PROTEUS_CPACK_COMP_DEV})
-set(_proteus_install_bin ${PROTEUS_CPACK_COMP_EXPERIMENTS})
-include(_proteus-install)
+  set(_proteus_install_dev ${PROTEUS_CPACK_COMP_DEV})
+  set(_proteus_install_bin ${PROTEUS_CPACK_COMP_EXPERIMENTS})
+  include(_proteus-install)
+endfunction()
